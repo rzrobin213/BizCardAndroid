ee40ef609b705d978444e87f52f85d2827ab538e
Robin Zheng <rjz47@cornell.edu>
2019-06-11 23:37:04 -0400

    First Commit Start

---
+++ app/src/main/java/com/example/bizcardandroid/AddUserActivity.java
@@ -0,0 +1,46 @@
+package com.example.bizcardandroid;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    Intent passedIntent = getIntent();
+    String code = passedIntent.getStringExtra("code");
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_add:
+                    return true;
+                case R.id.navigation_profile:
+                    finish();
+                    return true;
+                case R.id.navigation_contacts:
+                    Intent intent =
+                            new Intent(AddUserActivity.this, ContactView.class);
+                    intent.putExtra("code",code);
+                    startActivity(intent);
+                    finish();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setSelectedItemId(R.id.navigation_add);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
---
+++ app/src/main/java/com/example/bizcardandroid/ContactView.java
@@ -0,0 +1,44 @@
+package com.example.bizcardandroid;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ContactView extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_add:
+                    Intent intent =
+                            new Intent(ContactView.this, AddUserActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                case R.id.navigation_profile:
+                    finish();
+                    return true;
+                case R.id.navigation_contacts:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_view);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setSelectedItemId(R.id.navigation_contacts);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+}
